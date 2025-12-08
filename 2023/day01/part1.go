287e2ec528aaf245077105fa0c1fe93f4bcd6ed0
Liam G <liam.gallear@gmail.com>
2025-12-08 23:48:23 +0000

    2023 day01 optimised

--- 2023/day01/part1.go
+++ 2023/day01/part1.go
@@ -1,33 +1,27 @@
 package main
 
 import (
-	"aocli/utils"
 	"strings"
-	"sync"
 )
 
 func doPartOne(input string) int {
 	lines := strings.Split(strings.TrimSpace(input), "\n")
-	var wg sync.WaitGroup
-	ch := make(chan int, len(lines))
+	res := 0
 
 	for _, line := range lines {
-		wg.Add(1)
-		go func(line string, wg *sync.WaitGroup, ch chan int) {
-			defer wg.Done()
-			var res int
-			digits := findNums(line)
-			res = utils.Atoi(digits[0].num + digits[1].num)
-			ch <- res
-		}(line, &wg, ch)
+		var first, last byte
+		for i := 0; i < len(line); i++ {
+			if line[i] >= '0' && line[i] <= '9' {
+				if first == 0 {
+					first = line[i]
+				}
+				last = line[i]
+			}
+		}
+		if first != 0 {
+			res += int(first-'0')*10 + int(last-'0')
+		}
 	}
-	go func() {
-		wg.Wait()
-		close(ch)
-	}()
-	var res int
-	for out := range ch {
-		res += out
-	}
+
 	return res
 }
--- 2023/day01/part2.go
+++ 2023/day01/part2.go
@@ -1,66 +1,59 @@
 package main
 
 import (
-	"aocli/utils"
-	"slices"
 	"strings"
-	"sync"
 )
+
+var wordDigits = []struct {
+	word string
+	val  int
+}{
+	{"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5},
+	{"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9},
+}
 
 func doPartTwo(input string) int {
 	lines := strings.Split(strings.TrimSpace(input), "\n")
-	var wg sync.WaitGroup
-	ch := make(chan int, len(lines))
+	res := 0
+
 	for _, line := range lines {
-		wg.Add(1)
-		go func(line string, wg *sync.WaitGroup, ch chan int) {
-			defer wg.Done()
-			var res int
-			digits := findNums(line)
-			words := findWords(line)
-			if digits[0].id == -1 || (words[0].id != -1 && words[0].id < digits[0].id) {
-				digits[0] = words[0]
-			}
-			if words[1].id != -1 && words[1].id > digits[1].id {
-				digits[1] = words[1]
-			}
-			res = utils.Atoi(digits[0].num + digits[1].num)
-			ch <- res
-		}(line, &wg, ch)
+		first, last := findFirstAndLast(line)
+		res += first*10 + last
 	}
-	go func() {
-		wg.Wait()
-		close(ch)
-	}()
-	var res int
-	for out := range ch {
-		res += out
-	}
+
 	return res
 }
 
-func findWords(line string) Digits {
-	result := [2]digit{{-1, ""}, {-1, ""}}
-	charMap := make(map[int]string, 0)
-	var charIdx []int
+func findFirstAndLast(line string) (int, int) {
+	first, last := 0, 0
+	firstPos, lastPos := len(line), -1
 
-	for word, num := range digitMap {
-		first := strings.Index(line, word)
-		last := strings.LastIndex(line, word)
-		if first != -1 {
-			charIdx = append(charIdx, first)
-			charMap[first] = num
-		}
-		if last != -1 {
-			charIdx = append(charIdx, last)
-			charMap[last] = num
+	// Check for digit characters
+	for i := 0; i < len(line); i++ {
+		if line[i] >= '0' && line[i] <= '9' {
+			val := int(line[i] - '0')
+			if i < firstPos {
+				firstPos = i
+				first = val
+			}
+			if i > lastPos {
+				lastPos = i
+				last = val
+			}
 		}
 	}
 
-	if len(charIdx) > 0 {
-		slices.Sort(charIdx)
-		result[0] = digit{charIdx[0], charMap[charIdx[0]]}
-		result[1] = digit{charIdx[len(charIdx)-1], charMap[charIdx[len(charIdx)-1]]}
+	// Check for word digits
+	for _, wd := range wordDigits {
+		if idx := strings.Index(line, wd.word); idx != -1 && idx < firstPos {
+			firstPos = idx
+			first = wd.val
+		}
+		if idx := strings.LastIndex(line, wd.word); idx != -1 && idx > lastPos {
+			lastPos = idx
+			last = wd.val
+		}
 	}
-	return result
+
+	return first, last
 }
